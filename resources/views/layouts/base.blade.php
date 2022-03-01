02618b12bca67784930018beefa45cfde29721fc
Thomas Ostrowski <osthomos@gmail.com>
2022-03-01 15:12:11 +0100

    [+] Ajout de la déconnexion

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{ config('app.name') }}</title>
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
